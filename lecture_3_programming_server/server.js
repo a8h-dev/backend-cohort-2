ec0a93e3fd25aaa60c4a27a4999b64d935518152
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-03-11 16:22:29 +0530

    made another response for user request

--- lecture_3_programming_server/server.js
+++ lecture_3_programming_server/server.js
@@ -10,4 +10,13 @@ app.get('/about', (req, res) => {
     res.send("This is about page");
 })
 
+app.get('/home', (req, res) => {
+    res.send("This is Home Page");
+})
+
 app.listen(3000) // server start
+// 8080
+// 3000
+// 8000
+// 7000
+// 5193
