9a5ad412a6d1b6d14b67bee6cfbfa9fa00674851
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-03-16 20:55:11 +0530

    create notes and get all notes api done

--- lecture_5_rest_apis/src/app.js
+++ lecture_5_rest_apis/src/app.js
@@ -4,8 +4,22 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const notes = [];
+
 app.get("/", (req, res) => {
     res.send("Learning REST APIs");
+})
+
+app.post("/notes", (req, res) => {
+    notes.push(req.body);
+    console.log(notes);
+    res.send("Note Created");
+})
+
+app.get("/notes", (req, res) => {
+    res.send(notes);
 })
 
 module.exports = app;
