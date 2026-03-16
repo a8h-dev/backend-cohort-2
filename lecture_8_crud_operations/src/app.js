039c1c64bc1166e001cf3a577b48a1b79a3c2fbb
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-03-17 03:41:35 +0530

    fetch all notes using find method

--- lecture_8_crud_operations/src/app.js
+++ lecture_8_crud_operations/src/app.js
@@ -14,4 +14,12 @@ app.post("/notes", async (req, res) => {
     })
 })
 
+app.get("/notes", async (req, res) => {
+    const notes = await noteModel.find();
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes
+    })
+})
+
 module.exports = app;
