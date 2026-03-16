f1a6e00a83766f4b9def8e4756ff2fa81bdb2323
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-03-17 01:24:11 +0530

    connected to db

---
+++ lecture_8_crud_operations/notes.md
@@ -0,0 +1,5 @@
+cluster => storage + processor 
+database => use for storing data
+
+security layer on mongo => network access | database user
+
--- lecture_8_crud_operations/server.js
+++ lecture_8_crud_operations/server.js
@@ -1,4 +1,14 @@
+require("dotenv").config();
 const app = require("./src/app");
+const mongoose = require("mongoose");
+
+function connectDB() {
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("Connected to Database");
+    })
+}
+connectDB();
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
