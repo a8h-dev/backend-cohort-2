8d24df58cfad90217c3008e588efbafbe092da4b
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-03-17 16:38:36 +0530

    create note api created

--- lecture_9_fullstack/Backend/src/app.js
+++ lecture_9_fullstack/Backend/src/app.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const noteModel = require("./models/note.model");
+
+// POST /api/notes
+// create new note and save data in mongodb
+// req.body = {title, description}
+
+app.post("/api/notes", async (req, res) => {
+    const {title, description} = req.body;
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message: "Note created successfully",
+        note
+    })
+})
 
 module.exports = app;
