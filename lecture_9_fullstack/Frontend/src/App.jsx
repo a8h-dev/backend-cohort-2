5166323a749a7fc7df3337615647e6432e227390
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-03-17 18:42:54 +0530

    useEffect

--- lecture_9_fullstack/Frontend/src/App.jsx
+++ lecture_9_fullstack/Frontend/src/App.jsx
@@ -1,30 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
-
   const [notes, setNotes] = useState([]);
 
-  axios.get(`${import.meta.env.VITE_API_URL}/api/notes`)
-  .then((res)=>{
-    setNotes(res.data.notes)
-  })
+  console.log("Hello integration");
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_API_URL}/api/notes`)
+    .then((res) => {
+      setNotes(res.data.notes);
+    });
+  }, []);
 
   return (
     <>
-        <div className="notes">
-
-          {
-            notes.map((note, index)=>{
-              return <div key={index} className="note">
-            <h1>{note.title}</h1>
-            <p>{note.description}</p>
-          </div>
-            })
-          }
-        </div>
+      <div className="notes">
+        {notes.map((note, index) => {
+          return (
+            <div key={index} className="note">
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
-}
+};
 
 export default App;
