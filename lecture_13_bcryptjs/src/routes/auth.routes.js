133f139beb4507038104344f8941d4edcb3e4200
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-03-18 18:43:25 +0530

    login api created

--- lecture_13_bcryptjs/src/routes/auth.routes.js
+++ lecture_13_bcryptjs/src/routes/auth.routes.js
@@ -37,9 +37,40 @@ authRouter.post("/register", async (req, res) => {
   });
 });
 
-// /api/auth/protected
-authRouter.post("/protected", (req, res) => {
-  console.log(req.cookies);
+// // /api/auth/protected
+// authRouter.post("/protected", (req, res) => {
+//   console.log(req.cookies);
+// })
+
+// /api/auth/login
+// controller -> wo function jo tab execute hote hain jab api pe koi request aati hai
+authRouter.post("/login", async (req, res) => {
+  const {email, password} = req.body;
+  const user = await userModel.findOne({email});
+
+  if(!user){
+    return res.status(404).json({
+      message: `User not found with email: ${email}`,
+      clickHere: "Create new Account"
+    })
+  }
+
+  const isPasswordMatched = user.password === password;
+
+  if(!isPasswordMatched){
+    return res.status(401).json({
+      message: `Invalid Password: ${password}`
+    })
+  }
+
+  const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+
+  res.cookie("jwt_token", token);
+
+  res.status(200).json({
+    message: "User logged in successfully",
+    user,
+  })
 })
 
 module.exports = authRouter;
