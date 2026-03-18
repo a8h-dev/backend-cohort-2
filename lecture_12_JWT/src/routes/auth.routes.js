eb2153c9e82a0c1d350ff1c02ac1be84e2737064
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-03-18 15:16:38 +0530

    authRouter created and express.Router() use understood

---
+++ lecture_12_JWT/src/routes/auth.routes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+
+const authRouter = express.Router();
+// app.js file ke alawa agar kisi aur file me api create krni hai toh hame express.Router() ka use krna padta hai.
+
+module.exports = authRouter;
